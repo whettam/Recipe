c2714317dae9a042a8a2279edf4291b7ecb96d2e
whettam <mjballard88@gmail.com>
2024-02-05 10:17:56 -0600

    Initial commit

---
+++ app/src/main/java/edu/otc/recipe/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.otc.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
